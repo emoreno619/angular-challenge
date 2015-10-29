5ab437b07b3050e54937e9808c7a759f8d5e736f
Eduardo Moreno <emoreno@uchicago.edu>
2015-10-28 23:31:04 -0700

    progress bar and amounts all dynamic

--- index.js
+++ index.js
@@ -3,4 +3,9 @@ controller('driversController', function($scope) {
     $scope.budget = 250000.00
     $scope.contributed = 34569.23
     $scope.percentage = 100 * ($scope.contributed / $scope.budget)
+
+    $scope.add = function(){
+      $scope.contributed += parseFloat($scope.donation)
+      $scope.percentage = 100 * ($scope.contributed / $scope.budget)
+    }
 });
